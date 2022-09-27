96ffb111599776c6755702965a4875f16c8907d1
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-09-28 00:14:02 +0300

    completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function BlogContent(props) {
+  if (!props.articleText) {
+    return null
+  } else {
+    
+  
   return <div id="blog-content">{props.articleText}</div>;
+  }
 }
 
 export default BlogContent;
